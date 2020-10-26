3024ec90329b57437593a2da0b65f37e49137b2a
Peter217 <pedro.marcial.p@outlook.com>
2020-10-26 17:03:08 -0600

    Se integra Hystrix para tolerancia de fallas

--- src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -3,10 +3,12 @@ package com.formacionbdi.springboot.app.item;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 // RIBBON YA ESTA INTEGRADO EN EUREKA
 //@RibbonClient(name = "servicio-productos")
